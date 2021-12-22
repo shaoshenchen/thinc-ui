5e77df3821907b49683b397534b292ad87bec843
shaoshenchen <1600083715@qq.com>
2021-12-22 12:55:53 +0800

    new Menu component in storybook

--- src/components/Alert/alert.tsx
+++ src/components/Alert/alert.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import classNames from 'classnames'
-import Button, { ButtonType } from '../Button/button'
+import Button from '../Button/button'
 
 type AlertType = 'success' | 'info' | 'warning' | 'error'
 
---
+++ src/components/Menu/menu.stories.tsx
@@ -0,0 +1,32 @@
+import { storiesOf } from '@storybook/react'
+import Menu from './menu'
+import MenuItem from './menuItem'
+import SubMenu from './subMenu'
+
+const HorizontalMenu = () => (
+  <Menu>
+    <MenuItem>item - 0</MenuItem>
+    <MenuItem disabled>item - 1</MenuItem>
+    <SubMenu title='dropdown'>
+      <MenuItem>down - 1</MenuItem>
+      <MenuItem>down - 2</MenuItem>
+    </SubMenu>
+    <MenuItem>item - 2</MenuItem>
+  </Menu>
+)
+
+const VerticalMenu = () => (
+  <Menu mode='vertical'>
+    <MenuItem>item - 0</MenuItem>
+    <MenuItem disabled>item - 1</MenuItem>
+    <SubMenu title='dropdown'>
+      <MenuItem>down - 1</MenuItem>
+      <MenuItem>down - 2</MenuItem>
+    </SubMenu>
+    <MenuItem>item - 2</MenuItem>
+  </Menu>
+)
+
+storiesOf('Menu', module)
+  .add('水平 Menu', HorizontalMenu)
+  .add('垂直 Menu', VerticalMenu)
