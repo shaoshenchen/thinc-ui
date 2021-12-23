c1eabdc4e417656d164c6416442c5ecaed582efc
shaoshenchen <1600083715@qq.com>
2021-12-23 17:35:05 +0800

    add KeyboardEvent in AutoComplete(finished)

--- src/components/AutuComplete/autoComplete.tsx
+++ src/components/AutuComplete/autoComplete.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, KeyboardEvent, useEffect, useState } from "react";
+import { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from "react";
 import { FC } from "react";
 import Input, { InputProps } from "../Input/input";
 import Icon from "../Icon/icon";
@@ -21,72 +21,106 @@ interface SuggestionProps {
  * @returns {Array} list - 与 query 匹配的用户列表
  */
 const fetchSuggesstions = (query: string) => {
-  // 异步获取
-  return fetch(`https://api.github.com/search/users?q=${query}`)
-    .then(res => res.json())
-    .then(res => {
-      return res.items.slice(0, 10).map((item: any) => ({ value: item.login, ...item }))
-    })
+  // 从已知列表中获取
+  const products = [
+    'MacBook Air', 'MacBook Pro', 'iMac', 'Mac Pro', 'Mac mini',
+    'iPad Pro', 'iPad Air', 'iPad', 'iPad mini',
+    'iPhone 13', 'iPhone 13 Pro', 'iPhone 13 Pro Max',
+    'Apple Watch Serices 7', 'Apple Watch SE',
+    'AirPods 第三代', 'AirPods Pro', 'AirPods Max'
+  ]
+  const res = products.filter((item) => {
+    return item.toLocaleLowerCase().includes(query)
+  })
+  return res
+
+  // // 异步获取
+  // return fetch(`https://api.github.com/search/users?q=${query}`)
+  //   .then(res => res.json())
+  //   .then(res => {
+  //     return res.items.slice(0, 10).map((item: any) => ({ value: item.login, ...item }))
+  //   })
 }
 
 const AutoComplete: FC<AutoCompleteProps> = (props) => {
   const { value, onSelect, ...restProps } = props
   // value 如果值为 undefined，那么渲染的 Input 组件是非受控组件
   const [inputValue, setInputValue] = useState(value || '')
-  const [suggestions, setSuggestions] = useState<SuggestionProps[]>([])
+  const [suggestions, setSuggestions] = useState<string[]>([])
   const [loading, setloading] = useState(false)
   const [highlightIndex, setHighlightIndex] = useState(-1)
+  const triggerSelect = useRef(false)
   const debounceValue = useDebounce(inputValue)
 
   useEffect(() => {
-    if (debounceValue) {
+    if (debounceValue && triggerSelect.current) {
       // 将搜索关键字小写
       const formativeValue = debounceValue.trim().toLowerCase()
       const results = fetchSuggesstions(formativeValue)
-      results.then(data => {
-        setloading(false)
-        setSuggestions(data)
-      })
+
+      // 从已知列表获取
+      setloading(false)
+      setSuggestions(results)
+
+      // 异步获取
+      // results.then(data => {
+      //   setloading(false)
+      //   setSuggestions(data)
+      // })
     } else {
       setloading(false)
       setSuggestions([])
     }
+
+    setHighlightIndex(-1)
   }, [debounceValue])
+
+  const normalizeHighlightIndex = (idx: number) => {
+    // 限制上下边界
+    if (idx < 0) {
+      idx = 0
+    }
+    if (idx > suggestions.length - 1) {
+      idx = suggestions.length - 1
+    }
+
+    setHighlightIndex(idx)
+  }
 
   const handleKeyDown = (e: KeyboardEvent) => {
     switch (e.key) {
       case 'ArrowUp':
-        setHighlightIndex(highlightIndex - 1)
+        normalizeHighlightIndex(highlightIndex - 1)
         break
       case 'ArrowDown':
-        setHighlightIndex(highlightIndex + 1)
+        normalizeHighlightIndex(highlightIndex + 1)
         break
       case 'Enter':
+        handleClick(suggestions[highlightIndex])
         break
       case 'Escape':
+        setSuggestions([])
         break
       default:
         break
     }
-
-    // 限制上下边界
-    (highlightIndex < 0) && setHighlightIndex(0);
-    (highlightIndex > suggestions.length - 1) && setHighlightIndex(suggestions.length - 1);
-
   }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    // 输入框显示的值
+    // 输入框显示的值，不可在此修改数据
     const val = e.target.value
     setInputValue(val)
     setloading(true)
+    triggerSelect.current = true
   }
 
   // 点击 li 标签自动补全 Input
-  const handleClick = (item: SuggestionProps) => {
-    setInputValue(item.value)
+  const handleClick = (item: string) => {
+    setInputValue(item)
     setSuggestions([])
-    onSelect && onSelect(item.value)
+    triggerSelect.current = false
+    // 用户自定义事件
+    onSelect && onSelect(item)
   }
 
   const showUserList = () => {
@@ -102,11 +136,9 @@ const AutoComplete: FC<AutoCompleteProps> = (props) => {
           const classes = classNames('suggestion-item', {
             'suggestion-highlight': idx === highlightIndex
           })
-
           return (
             <li key={idx} onClick={() => handleClick(item)} className={classes}>
-              <h2>{item.value}</h2>
-              <p>{item.id}</p>
+              {item}
             </li>
           )
         })
